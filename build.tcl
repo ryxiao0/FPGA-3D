197e29cdcf03b77ceba12eaa673b9f0eaa57e570
Jessica Pan <jnpan@mit.edu>
2023-12-07 21:44:18 -0500

    float_to_screen map

--- build.tcl
+++ build.tcl
@@ -30,7 +30,8 @@ set_part $partNum
 read_ip ./ip/multiplier/multiplier.xci
 read_ip ./ip/adder/adder.xci
 read_ip ./ip/fifo/fifo.xci
-read_ip ./ip/reciprocal/reciprocal.xci
+# read_ip ./ip/reciprocal/reciprocal.xci
+# read_ip ./ip/float_to_fixed/float_to_fixed.xci
 generate_target all [get_ips]
 synth_ip [get_ips]
 
--- hdl/float_to_screen_map.sv
+++ hdl/float_to_screen_map.sv
@@ -1,72 +1,83 @@
-/*`default_nettype none
+
+`default_nettype none
 `timescale 1ns/1ps
 module float_to_screen_map
     (
         input wire clk_in,
         input wire rst_in,
-        input wire valid_in,
+        input wire data_valid_in,
         input wire [31:0] input_float,
         output logic data_valid_out,
+        output logic [31:0] float_out, 
         output logic [31:0] output_integer
     );
 
     logic [31:0] float_180 = 32'b01000011001101000000000000000000;
-    logic [31:0] input_times_180;
+    logic [31:0] float_one = 32'b00111111100000000000000000000000;
+    logic [31:0] float_zero_to_one;
     logic [31:0] float_0_to_360;
-    logic [31:0] final_integer;
+    logic [31:0] f2f_output;
+    logic add_valid_out;
     logic mult_valid_out;
-    logic float_valid_out;
-
-    multiplier mult (
-        .aclk(clk_in),
-        .s_axis_a_tdata(input_float),
-        .s_axis_a_tready(),
-        .s_axis_a_tvalid(valid_in),
-        .s_axis_b_tdata(float_180),
-        .s_axis_b_tready(),
-        .s_axis_b_tvalid(1'b1),
-        .m_axis_result_tdata(input_times_180),
-        .m_axis_result_tready(1'b1),
-        .m_axis_result_tvalid(mult_valid_out)
-    );
+    logic f2f_data_valid_out;
+    logic f2f_is_ready_for_mult;
+    logic mult_is_ready_for_add;
 
     adder add (
         .aclk(clk_in),
-        .s_axis_a_tdata(input_times_180),
-        .s_axis_a_tready(),
-        .s_axis_a_tvalid(mult_valid_out),
+        .s_axis_a_tdata(input_float),
+        .s_axis_a_tready(1'b1),
+        .s_axis_a_tvalid(data_valid_in),
+        .s_axis_b_tdata(float_one),
+        .s_axis_b_tready(1'b1),
+        .s_axis_b_tvalid(1'b1),
+        .m_axis_result_tdata(float_zero_to_one),
+        .m_axis_result_tready(mult_is_ready_for_add),
+        .m_axis_result_tvalid(add_valid_out)
+    );
+
+    multiplier mult (
+        .aclk(clk_in),
+        .s_axis_a_tdata(float_zero_to_one),
+        .s_axis_a_tready(mult_is_ready_for_add),
+        .s_axis_a_tvalid(add_valid_out),
         .s_axis_b_tdata(float_180),
         .s_axis_b_tready(),
         .s_axis_b_tvalid(1'b1),
         .m_axis_result_tdata(float_0_to_360),
-        .m_axis_result_tready(1'b1),
-        .m_axis_result_tvalid(float_valid_out)
+        .m_axis_result_tready(f2f_is_ready_for_mult),
+        .m_axis_result_tvalid(mult_valid_out)
+    );
+
+    float_to_fixed float_to_fixed_map (
+        .aclk(clk_in),
+        .s_axis_a_tvalid(mult_valid_out),
+        .s_axis_a_tready(f2f_is_ready_for_mult),
+        .s_axis_a_tdata(float_0_to_360),
+        .m_axis_result_tvalid(f2f_data_valid_out),
+        .m_axis_result_tready(1),
+        .m_axis_result_tdata(f2f_output)
     );
 
     always_ff @(posedge clk_in) begin
         if (rst_in) begin
-            input_times_180 <= 0;
-            float_0_to_360 <= 0;
-            final_integer <= 0;
-            mult_valid_out <= 0; 
-            float_valid_out <= 0;
             data_valid_out <= 0;
+            float_out <= 0;
+            output_integer <= 0;
         end else begin 
-            // convert float_0_to_360 to an integer 
-            // int'(float_0_to_360) would also work if this doesn't
-            if(float_valid_out) begin
-                final_integer <= $rtoi(float_0_to_360);
-                data_valid_out <= 1;
-            end else begin
-                data_valid_out <= 0;
+            data_valid_out <= f2f_data_valid_out;
+            if(f2f_data_valid_out) begin
+                output_integer <= f2f_output >> 6;
+            end 
+            if(mult_valid_out) begin
+                float_out <= float_0_to_360;
             end
 
         end
     end
 
-    assign output_integer = final_integer;
 
 
 
 endmodule
-`default_nettype wire*/
+`default_nettype wire
