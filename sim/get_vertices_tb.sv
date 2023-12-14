aca1f7a9524455d98064b37da118a2e2c7d6e0e6
Ryan Xiao <ryxiao36@gmail.com>
2023-12-13 21:21:31 -0500

    get vertices tb

--- sim/get_vertices_tb.sv
+++ sim/get_vertices_tb.sv
@@ -6,17 +6,37 @@ module get_vertices_tb;
     logic clk;
     logic rst;
     logic go;
-    logic [31:0] tri_out [3:0] [2:0];
+    logic [31:0] v1 [3:0];
+    logic [31:0] v2 [3:0];
+    logic [31:0] v3 [3:0];
     logic valid_out;
     logic obj_done;
+    logic ready_in;
+    logic [31:0] v1_x, v1_y, v1_z;
+    logic [31:0] v2_x, v2_y, v2_z;
+    logic [31:0] v3_x, v3_y, v3_z;
+
+    assign v1_x = v1[3];
+    assign v1_y = v1[2];
+    assign v1_z = v1[1];
+
+    assign v2_x = v2[3];
+    assign v2_y = v2[2];
+    assign v2_z = v2[1];
+
+    assign v3_x = v3[3];
+    assign v3_y = v3[2];
+    assign v3_z = v3[1];
 
     get_vertices uut (
         .clk_in(clk),
         .rst_in(rst),
-        .go(go),
-        .tri_out(tri_out),
+        .v1(v1),
+        .v2(v2),
+        .v3(v3),
         .valid_out(valid_out),
-        .obj_done(obj_done)
+        .obj_done(obj_done),
+        .ready_in(ready_in)
     );
 
     always begin
@@ -34,9 +54,9 @@ module get_vertices_tb;
         rst = 1;
         #10;
         rst = 0;
-        go = 1;
+        ready_in = 1;
         #10;
-        go = 0;
+        // go = 0;
         #10000;
         $display("Finishing Sim");;
         $finish;
